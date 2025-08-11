4d95927ed30d2e9ca4f12851e6552058e6cef834
Abdullah Shakur <109961338+abdullahshakur149@users.noreply.github.com>
2025-08-11 13:09:35 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KNKTG93V');</script>
         {/* Google tag (gtag.js) */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-P58FPZ20R8"></script>
         <script
@@ -35,6 +40,8 @@ export default function RootLayout({
       </head>
       <body className={fontSans.variable}>
         {children}
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KNKTG93V"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         {/* Start of Waypoint Converts Navigator Config Code */}
         <script
           dangerouslySetInnerHTML={{
